bbd22dd5804e15f11f198c9ee3cabb84d0b6be52
wonchul <onedang22@gmail.com>
2023-01-07 01:14:23 +0900

    fit and clt are integrated

--- 1_train_fit.py
+++ 1_train_fit.py
@@ -10,6 +10,9 @@ from utils.helpers import visualize
 from src.datasets import CamvidDataset, Dataloader
 from utils.augment import get_training_augmentation, get_preprocessing, get_validation_augmentation
 
+# train_mode = 'fit'
+train_mode = 'clt'
+
 ### configurate GPUs settings
 # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -70,8 +73,7 @@ optim = tf.keras.optimizers.Adam(LR)
 dice_loss = sm.losses.DiceLoss(class_weights=np.array([1, 2, 0.5])) 
 focal_loss = sm.losses.BinaryFocalLoss() if n_classes == 1 else sm.losses.CategoricalFocalLoss()
 total_loss = dice_loss + (1 * focal_loss)
-metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
-model.compile(optim, total_loss, metrics)
+
 
 ### define datalader for train images
 train_dataset = CamvidDataset(
@@ -97,21 +99,112 @@ valid_dataloader = Dataloader(valid_dataset, batch_size=1, shuffle=False)
 assert train_dataloader[0][0].shape == (BATCH_SIZE, input_height, input_width, 3)
 assert train_dataloader[0][1].shape == (BATCH_SIZE, input_height, input_width, n_classes)
 
-# define callbacks for learning rate scheduling and best checkpoints saving
-callbacks = [
-    tf.keras.callbacks.ModelCheckpoint(osp.join(ckpt_results, 'best_model.h5'), save_weights_only=True, save_best_only=True, mode='min'),
-    tf.keras.callbacks.ReduceLROnPlateau(),
-]
 
-# train model
-history = model.fit(
-    train_dataloader, 
-    steps_per_epoch=len(train_dataloader), 
-    epochs=EPOCHS, 
-    callbacks=callbacks, 
-    validation_data=valid_dataloader, 
-    validation_steps=len(valid_dataloader),
-)
+
+if train_mode == 'fit':
+    metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+    model.compile(optim, total_loss, metrics)
+    
+    # define callbacks for learning rate scheduling and best checkpoints saving
+    callbacks = [
+        tf.keras.callbacks.ModelCheckpoint(osp.join(ckpt_results, 'best_model.h5'), save_weights_only=True, save_best_only=True, mode='min'),
+        tf.keras.callbacks.ReduceLROnPlateau(),
+    ]
+    history = model.fit(
+        train_dataloader, 
+        steps_per_epoch=len(train_dataloader), 
+        epochs=EPOCHS, 
+        callbacks=callbacks, 
+        validation_data=valid_dataloader, 
+        validation_steps=len(valid_dataloader),
+    )
+elif train_mode == 'clt':
+    metrics = sm.metrics.IOUScore(threshold=0.5)
+
+    @tf.function
+    def train_step(x, y):
+        with tf.GradientTape() as tape:
+            preds = model(x)
+            # preds = tf.cast(preds, tf.float32)
+            y = tf.cast(y, tf.float32)
+            loss = total_loss(y, preds)
+            iou = metrics(y, preds)
+
+        grads = tape.gradient(loss, model.trainable_weights)
+        optim.apply_gradients(zip(grads, model.trainable_weights))
+
+        return loss, iou
+
+    @tf.function
+    def validation_step(x, y):
+        preds = model(x)
+        y = tf.cast(y, tf.float32)
+        test_loss = total_loss(y, preds)
+        test_iou = metrics(y, preds)
+
+        return test_loss, test_iou
+
+
+    train_steps = len(train_dataset)//BATCH_SIZE
+    valid_steps = len(valid_dataset)//BATCH_SIZE
+
+    if len(train_dataset) % BATCH_SIZE != 0:
+        train_steps += 1
+    if len(valid_dataset) % BATCH_SIZE != 0:
+        valid_steps += 1
+
+
+    TRAIN_LOSSES = []
+    TRAIN_IOU_SCORES = []
+    VAL_LOSSES = []
+    VAL_IOU_SCORES = []
+    best_iou_score = 0.0
+    for epoch in range(300):
+        # initializes training losses and iou_scores lists for the epoch
+        losses = []
+        iou_scores = []
+
+        for step, (batch) in enumerate(train_dataloader):
+
+            x, y = batch[0], batch[1]
+            # run one training step for the current batch
+            loss, iou = train_step(x, y)
+
+            # Save current batch loss and iou-score
+            losses.append(float(loss))
+            iou_scores.append(float(iou))
+
+            print("\r Epoch: {} >> step: {}/{} >> train-loss: {} >> IOU: {}".format(epoch, step, train_steps, \
+                                np.round(sum(losses) / len(losses), 4), np.round(sum(iou_scores) / len(iou_scores), 4)), end="")
+
+        # Save the train and validation losses and iou scores for each epoch.
+        TRAIN_LOSSES.append(sum(losses) / len(losses))
+        TRAIN_IOU_SCORES.append(sum(iou_scores) / len(iou_scores))
+            
+        if epoch % 1 == 0 and epoch != 0:
+            val_losses = []
+            val_iou_scores = []
+            for val_step, val_batch in enumerate(valid_dataloader):
+                x_val, y_val = val_batch[0], val_batch[1]
+
+                # val_loss, val_iou_score = distributed_val_step(x_val, y_val)
+                val_loss, val_iou_score = validation_step(x_val, y_val)
+
+                val_losses.append(val_loss)
+                val_iou_scores.append(val_iou_score)
+
+                print("** \rEpoch: {} >> Val_Loss: {} >> Val_IOU-Score: {} ".format(epoch, np.round(sum(val_losses) / len(val_losses), 4), \
+                                    np.round(sum(val_iou_scores) / len(val_iou_scores), 4)), end="")
+                    
+            if sum(val_iou_scores) / len(val_iou_scores) > best_iou_score:
+                best_iou_score = sum(val_iou_scores) / len(val_iou_scores)
+                model.save_weights(osp.join(ckpt_results, 'best_model.h5'))
+
+
+            VAL_LOSSES.append(sum(val_losses) / len(val_losses))
+            VAL_IOU_SCORES.append(sum(val_iou_scores) / len(val_iou_scores))
+            
+        print()
 
 # Plot training & validation iou_score values
 plt.figure(figsize=(30, 5))
