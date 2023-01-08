6fda1717cac0ac2cbff49ce6a214501ac32ef387
wonchul <onedang22@gmail.com>
2023-01-08 15:44:39 +0900

    update

--- test.py
+++ test.py
@@ -4,12 +4,19 @@ from utils.augment import  get_preprocessing, get_validation_augmentation
 from utils.helpers import visualize, denormalize
 
 import segmentation_models as sm 
+import tensorflow_advanced_segmentation_models as tasm
+from keras_unet_collection._model_swin_unet_2d import swin_unet_2d
+from keras_unet_collection import models, base
+from tensorflow.keras.layers import Input, Conv2D, Dropout, Activation, UpSampling2D, GlobalMaxPooling2D, multiply
 import tensorflow as tf 
+from tensorflow.keras.backend import max
+
 import keras 
 import numpy as np 
 
-train_mode = 'fit'
-train_mode = 'ctl'
+# train_mode = 'fit'
+# train_mode = 'ctl'
+train_mode = 'ctl_multigpus'
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
@@ -32,7 +39,7 @@ n_classes = 1 if len(CLASSES) == 1 else (len(CLASSES) + 1)  # case for binary an
 activation = 'sigmoid' if n_classes == 1 else 'softmax'
 preprocess_input = sm.get_preprocessing(BACKBONE)
 
-input_height, input_width, input_channel = 128, 128, 3
+input_height, input_width, input_channel = 256, 256, 3
 test_dataset = CamvidDataset(
     x_test_dir, 
     y_test_dir, 
@@ -42,7 +49,120 @@ test_dataset = CamvidDataset(
 )
 test_dataloader = Dataloader(test_dataset, batch_size=1, shuffle=False)
 
-model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
+# model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
+
+# base_model, layers, layer_names = tasm.create_base_model(name=BACKBONE, weights="imagenet", \
+#             height=input_height, width=input_width, include_top=False, pooling=None)
+
+# BACKBONE_TRAINABLE = False
+# MODEL_PATH = "danet"
+# model = tasm.DANet(n_classes=n_classes, base_model=base_model, output_layers=layers, \
+#     backbone_trainable=BACKBONE_TRAINABLE)
+
+
+# ########## unet-series
+# filter_num_begin = 128     # number of channels in the first downsampling block; it is also the number of embedded dimensions
+# depth = 4                  # the depth of SwinUNET; depth=4 means three down/upsampling levels and a bottom level 
+# stack_num_down = 2         # number of Swin Transformers per downsampling level
+# stack_num_up = 2           # number of Swin Transformers per upsampling level
+# patch_size = (4, 4)        # Extract 4-by-4 patches from the input image. Height and width of the patch must be equal.
+# num_heads = [4, 8, 8, 8]   # number of attention heads per down/upsampling level
+# window_size = [4, 2, 2, 2] # the size of attention window per down/upsampling level
+# num_mlp = 512              # number of MLP nodes within the Transformer
+# shift_window=True          # Apply window shifting, i.e., Swin-MSA
+
+# n_labels = n_classes
+
+# # Input section
+# input_size = (input_height, input_width, input_channel)
+# # IN = Input(input_size)
+
+# # # Base architecture
+# # X = swin_unet_2d_base(IN, filter_num_begin, depth, stack_num_down, stack_num_up, 
+# #                       patch_size, num_heads, window_size, num_mlp, 
+# #                       shift_window=shift_window, name='swin_unet')
+
+# # # Output section
+# # OUT = Conv2D(n_labels, kernel_size=1, use_bias=False, activation='softmax')(X)
+
+# # # Model configuration
+# # model = Model(inputs=[IN,], outputs=[OUT,])
+
+# model = swin_unet_2d(input_size=input_size, filter_num_begin=filter_num_begin, n_labels=n_labels, depth=depth, \
+#                     stack_num_down=stack_num_down, stack_num_up=stack_num_up, patch_size=patch_size, num_heads=num_heads, \
+#                     window_size=window_size, num_mlp=num_mlp, shift_window=shift_window, name='swin_unet')
+
+# optim = tf.keras.optimizers.Adam(learning_rate=1e-4, clipvalue=0.5)
+
+
+# ########## unet-series
+name = 'unet3plus'
+activation = 'ReLU'
+filter_num_down = [32, 64, 128, 256, 512]
+filter_num_skip = [32, 32, 32, 32]
+filter_num_aggregate = 160
+
+stack_num_down = 2
+stack_num_up = 1
+n_labels = n_classes
+
+# `unet_3plus_2d_base` accepts an input tensor 
+# and produces output tensors from different upsampling levels
+# ---------------------------------------- #
+input_tensor = tf.keras.layers.Input((input_height, input_width, input_channel))
+# base architecture
+X_decoder = base.unet_3plus_2d_base(
+    input_tensor, filter_num_down, filter_num_skip, filter_num_aggregate, 
+    stack_num_down=stack_num_down, stack_num_up=stack_num_up, activation=activation, 
+    batch_norm=True, pool=True, unpool=True, backbone=None, name=name)
+
+OUT_stack = []
+# reverse indexing `X_decoder`, so smaller tensors have larger list indices 
+X_decoder = X_decoder[::-1]
+
+# deep supervision outputs
+for i in range(1, len(X_decoder)):
+    # 3-by-3 conv2d --> upsampling --> sigmoid output activation
+    pool_size = 2**(i)
+    X = Conv2D(n_labels, 3, padding='same', name='{}_output_conv1_{}'.format(name, i-1))(X_decoder[i])
+    
+    X = UpSampling2D((pool_size, pool_size), interpolation='bilinear', 
+                    name='{}_output_sup{}'.format(name, i-1))(X)
+    
+    X = Activation('sigmoid', name='{}_output_sup{}_activation'.format(name, i-1))(X)
+    # collecting deep supervision tensors
+    OUT_stack.append(X)
+
+# the final output (without extra upsampling)
+# 3-by-3 conv2d --> sigmoid output activation
+X = Conv2D(n_labels, 3, padding='same', name='{}_output_final'.format(name))(X_decoder[0])
+X = Activation('sigmoid', name='{}_output_final_activation'.format(name))(X)
+# collecting final output tensors
+OUT_stack.append(X)
+
+# Classification-guided Module (CGM)
+# ---------------------------------------- #
+# dropout --> 1-by-1 conv2d --> global-maxpooling --> sigmoid
+X_CGM = X_decoder[-1]
+X_CGM = Dropout(rate=0.1)(X_CGM)
+X_CGM = Conv2D(filter_num_skip[-1], 1, padding='same')(X_CGM)
+X_CGM = GlobalMaxPooling2D()(X_CGM)
+X_CGM = Activation('sigmoid')(X_CGM)
+
+CGM_mask = max(X_CGM, axis=-1) # <----- This value could be trained with "none-organ image"
+
+for i in range(len(OUT_stack)):
+    if i < len(OUT_stack)-1:
+        # deep-supervision
+        OUT_stack[i] = multiply([OUT_stack[i], CGM_mask], name='{}_output_sup{}_CGM'.format(name, i))
+    else:
+        # final output
+        OUT_stack[i] = multiply([OUT_stack[i], CGM_mask], name='{}_output_final_CGM'.format(name))
+
+
+model = tf.keras.models.Model([input_tensor,], OUT_stack)
+optim = tf.keras.optimizers.Adam(learning_rate=1e-4)
+
 
 optim = keras.optimizers.Adam(LR)
 dice_loss = sm.losses.DiceLoss(class_weights=np.array([1, 2, 0.5])) 
@@ -50,6 +170,7 @@ focal_loss = sm.losses.BinaryFocalLoss() if n_classes == 1 else sm.losses.Catego
 total_loss = dice_loss + (1 * focal_loss)
 metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
 model.compile(optim, total_loss, metrics)
+model.build(input_shape=(None, input_height, input_width, input_channel))
 
 # load best weights
 model.load_weights('./results/checkpoints/{}_best_model.h5'.format(train_mode)) 
@@ -68,5 +189,5 @@ for i in ids:
     pr_mask = model.predict(image)
     
     visualize({"image": denormalize(image.squeeze()), \
-            "gt_mask": gt_mask.squeeze(), "pr_mask": pr_mask.squeeze()}, \
+            "gt_mask": gt_mask, "pr_mask": pr_mask.squeeze()}, \
             fp='./results/figs/{}_res_{}.png'.format(train_mode, i))
--- train.py
+++ train.py
@@ -5,13 +5,24 @@ import matplotlib.pyplot as plt
 
 import tensorflow as tf 
 
+from pidnet.pidnet import PIDNet
+
 import segmentation_models as sm
+import tensorflow_advanced_segmentation_models as tasm
+from keras_unet_collection._model_swin_unet_2d import swin_transformer_stack, swin_unet_2d_base
+import keras_unet_collection.utils as utils
+from keras_unet_collection._model_swin_unet_2d import swin_unet_2d
+from keras_unet_collection import models, base
+from tensorflow.keras.layers import Input, Conv2D, Dropout, Activation, UpSampling2D, GlobalMaxPooling2D, multiply
+
 from utils.helpers import visualize, denormalize
 from src.datasets import CamvidDataset, Dataloader
 from utils.augment import get_training_augmentation, get_preprocessing, get_validation_augmentation
+from tensorflow.keras.backend import max
 
 # train_mode = 'fit'
-train_mode = 'ctl'
+# train_mode = 'ctl'
+train_mode = 'ctl_multigpus'
 
 ### configurate GPUs settings
 # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
@@ -43,7 +54,8 @@ y_train_dir = os.path.join(DATA_DIR, 'train/masks')
 x_valid_dir = os.path.join(DATA_DIR, 'val/images')
 y_valid_dir = os.path.join(DATA_DIR, 'val/masks')
 
-input_height, input_width, input_channel = 480, 480, 3
+input_height, input_width, input_channel = 256, 256, 3
+
 tmp_dataset = CamvidDataset(x_train_dir, y_train_dir, classes=CLASSES, \
                 augmentation=get_training_augmentation(input_height, input_width))
 
@@ -62,7 +74,7 @@ activation = 'sigmoid' if n_classes == 1 else 'softmax'
 BACKBONE = 'efficientnetb0'
 BATCH_SIZE = 2
 LR = 0.0001
-EPOCHS = 10
+EPOCHS = 50
 
 preprocess_input = sm.get_preprocessing(BACKBONE)
 print(preprocess_input)
@@ -231,9 +243,6 @@ elif train_mode == 'ctl':
                     _preds = model(_x_val)
                     visualize({"image" : denormalize(_x_val.squeeze()), "gt_mask": _y_val.squeeze(), \
                         "pr_mask": _preds.numpy().squeeze()}, fp=osp.join(vis_results, 'val_{}_{}.png'.format(epoch, _val_step)))
-
-
-
                 print("------------------------------------------")
             VAL_LOSSES.append(sum(val_losses) / len(val_losses))
             VAL_IOU_SCORES.append(sum(val_iou_scores) / len(val_iou_scores))
@@ -260,3 +269,189 @@ elif train_mode == 'ctl':
     plt.legend(['Train', 'Test'], loc='upper left')
     plt.savefig("./results/figs/{}_res_{}.png".format(train_mode, EPOCHS))
 
+elif train_mode == 'ctl_multigpus':
+    strategy = tf.distribute.MirroredStrategy()
+    print('* Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+    with strategy.scope():
+        ######### sm
+        # model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)   
+        # optim = tf.keras.optimizers.Adam(LR)
+
+        # ######### tasm    
+        # base_model, layers, layer_names = tasm.create_base_model(name=BACKBONE, weights="imagenet", \
+        #             height=input_height, width=input_width, include_top=False, pooling=None)
+
+        # BACKBONE_TRAINABLE = False
+        # MODEL_PATH = "danet"
+        # model = tasm.DANet(n_classes=n_classes, base_model=base_model, output_layers=layers, \
+        #     backbone_trainable=BACKBONE_TRAINABLE)
+
+        # optim = tf.keras.optimizers.SGD(learning_rate=0.2, momentum=0.9)
+        # # iou_score = tasm.metrics.IOUScore(threshold=0.5)
+        # # categorical_focal_dice_loss = tasm.losses.CategoricalFocalLoss(alpha=0.25, gamma=2.0) + tasm.losses.DiceLoss(class_weights=class_weights)
+    
+        # ########## unet-series
+        # filter_num_begin = 128     # number of channels in the first downsampling block; it is also the number of embedded dimensions
+        # depth = 4                  # the depth of SwinUNET; depth=4 means three down/upsampling levels and a bottom level 
+        # stack_num_down = 2         # number of Swin Transformers per downsampling level
+        # stack_num_up = 2           # number of Swin Transformers per upsampling level
+        # patch_size = (4, 4)        # Extract 4-by-4 patches from the input image. Height and width of the patch must be equal.
+        # num_heads = [4, 8, 8, 8]   # number of attention heads per down/upsampling level
+        # window_size = [4, 2, 2, 2] # the size of attention window per down/upsampling level
+        # num_mlp = 512              # number of MLP nodes within the Transformer
+        # shift_window=True          # Apply window shifting, i.e., Swin-MSA
+
+        # n_labels = n_classes
+
+        # # Input section
+        # input_size = (input_height, input_width, input_channel)
+        # # IN = Input(input_size)
+
+        # # # Base architecture
+        # # X = swin_unet_2d_base(IN, filter_num_begin, depth, stack_num_down, stack_num_up, 
+        # #                       patch_size, num_heads, window_size, num_mlp, 
+        # #                       shift_window=shift_window, name='swin_unet')
+
+        # # # Output section
+        # # OUT = Conv2D(n_labels, kernel_size=1, use_bias=False, activation='softmax')(X)
+
+        # # # Model configuration
+        # # model = Model(inputs=[IN,], outputs=[OUT,])
+
+        # model = swin_unet_2d(input_size=input_size, filter_num_begin=filter_num_begin, n_labels=n_labels, depth=depth, \
+        #                     stack_num_down=stack_num_down, stack_num_up=stack_num_up, patch_size=patch_size, num_heads=num_heads, \
+        #                     window_size=window_size, num_mlp=num_mlp, shift_window=shift_window, name='swin_unet')
+
+        # optim = tf.keras.optimizers.Adam(learning_rate=1e-4, clipvalue=0.5)
+
+        # ########## pidnet
+
+
+        ### -------------------------------------------------------------------------------------------------
+        dice_loss = sm.losses.DiceLoss(class_weights=np.array([1, 2, 0.5])) 
+        focal_loss = sm.losses.BinaryFocalLoss() if n_classes == 1 else sm.losses.CategoricalFocalLoss()
+        metrics = dice_loss + (1 * focal_loss)
+        cal_iou = sm.metrics.IOUScore(threshold=0.5)
+
+
+        
+    def train_generator():
+        multi_enqueuer = tf.keras.utils.OrderedEnqueuer(train_dataloader, use_multiprocessing=False)
+        multi_enqueuer.start(workers=4, max_queue_size=10)
+        for _ in range(len(train_dataloader)):
+            batch_xs, batch_ys = next(multi_enqueuer.get())
+            yield batch_xs, batch_ys
+
+    _train_dataset = tf.data.Dataset.from_generator(train_generator,
+                                            output_types=(tf.float64, tf.float64),
+                                            output_shapes=(tf.TensorShape([None, None, None, None]),
+                                                            tf.TensorShape([None, None, None, None]))
+                                            )
+    valid_dataloader = Dataloader(valid_dataset, batch_size=2, shuffle=False)
+
+    def valid_generator():
+        multi_enqueuer = tf.keras.utils.OrderedEnqueuer(valid_dataloader, use_multiprocessing=False)
+        multi_enqueuer.start(workers=4, max_queue_size=10)
+        for _ in range(len(valid_dataloader)):
+            batch_xs, batch_ys = next(multi_enqueuer.get()) # I have three outputs
+            yield batch_xs, batch_ys
+
+    _valid_dataset = tf.data.Dataset.from_generator(valid_generator,
+                                            output_types=(tf.float64, tf.float64),
+                                            output_shapes=(tf.TensorShape([None, None, None, None]),
+                                                            tf.TensorShape([None, None, None, None]))
+                                            )
+
+    train_dist_dataset = strategy.experimental_distribute_dataset(_train_dataset)
+    valid_dist_dataset = strategy.experimental_distribute_dataset(_valid_dataset)
+
+    val_unscaled_loss = tf.keras.metrics.Sum(name='val_loss')
+    val_unscaled_iou = tf.keras.metrics.Sum(name='val_iou')
+
+    @tf.function
+    def train_step(batch):
+        image, label = batch 
+        label = tf.cast(label, tf.float32)
+        with tf.GradientTape() as tape:
+            preds = model(image)
+            loss = metrics(label, preds)
+            iou = cal_iou(label, preds)
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optim.apply_gradients(zip(gradients, model.trainable_variables))
+
+
+        # print("TRAINTRAIN -- unscaled_loss: ", loss, type(loss))
+        # print(loss.numpy())
+        return loss, iou
+
+    @tf.function
+    def distributed_train_epoch(ds):
+        total_loss = 0.
+        total_iou = 0.
+        num_train_batches = 0.
+        for batch in ds:
+            per_replica_loss, per_replica_iou = strategy.run(train_step, args=(batch,))
+            loss = strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_loss, axis=None)
+            iou = strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_iou, axis=None)
+            total_loss += loss
+            total_iou += iou
+
+            num_train_batches += 1
+        
+        return total_loss, total_iou, num_train_batches
+
+    @tf.function
+    def val_step(batch):
+        image, label = batch 
+        label = tf.cast(label, tf.float32)
+        preds = model(image)
+        unscaled_loss = metrics(label, preds)
+        unscaled_iou = cal_iou(label, preds)
+        # print("VALVAL -- unscaled_loss: ", unscaled_loss, type(unscaled_loss))
+        # print(unscaled_loss.numpy())
+        val_unscaled_loss(unscaled_loss)
+        val_unscaled_iou(unscaled_iou)
+
+    @tf.function
+    def distributed_val_epoch(ds):
+        num_val_batches = 0.
+        for batch in ds:
+            # print("------------------------------------------------------------idx: ", idx)
+            strategy.run(val_step, args=(batch,))
+            # print("----------------------------------------------------------------")
+            num_val_batches += 1
+
+        return val_unscaled_loss.result(), val_unscaled_iou.result(), num_val_batches
+        
+
+    TRAIN_LOSSES = []
+    TRAIN_IOU_SCORES = []
+    VAL_LOSSES = []
+    VAL_IOU_SCORES = []
+    best_iou_score = 0.0
+    best_val_loss = 999
+    for epoch in range(EPOCHS):
+        train_total_loss, train_total_iou, num_train_batches = distributed_train_epoch(train_dist_dataset)
+        val_total_loss, val_total_iou, num_val_batches = distributed_val_epoch(valid_dist_dataset)
+        train_avg_loss = train_total_loss / num_train_batches / strategy.num_replicas_in_sync
+        train_avg_iou = train_total_iou / num_train_batches / strategy.num_replicas_in_sync
+
+        val_avg_loss = val_total_loss / num_val_batches / strategy.num_replicas_in_sync
+        val_avg_iou = val_total_iou / num_val_batches / strategy.num_replicas_in_sync
+
+        print('>> Epoch: {}, Train Loss: {}, Train IoU: {}, Val Loss: {}, Val IoU: {}'.format(epoch, train_avg_loss, \
+                                                                                    train_avg_iou, val_avg_loss, val_avg_iou))
+
+        if best_val_loss > val_avg_loss:
+            best_val_loss = val_avg_loss
+            model.save_weights(osp.join(ckpt_results, '{}_best_model.h5'.format(train_mode)))
+
+        val_unscaled_iou.reset_states()
+        val_unscaled_loss.reset_states()
+
+
+        # if sum(val_iou_scores) / len(val_iou_scores) > best_iou_score:
+        #     best_iou_score = sum(val_iou_scores) / len(val_iou_scores)
+        #     model.save_weights("files/model.h5")
